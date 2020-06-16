63e7662116200d4b931185d5dde14ce81143d8fd
ydPro-G <46178109+ydPro-G@users.noreply.github.com>
2020-06-16 17:10:18 +0800

    类
    通过实例直接修改属性的值
    通过方法对属性的值进行设置
    通过方法对属性的值进行递增

--- python/9_class/9.2/use_class&instance.py
+++ python/9_class/9.2/use_class&instance.py
@@ -23,7 +23,7 @@ class Car():
         """返回整洁的描述性信息"""
         # 方法
         long_name = str(self.year) + ' ' + self.make + ' ' + self.model # 使用属性创建一个字符串
-        return long_name.title()   # 使用
+        return long_name.title()   # 使用return 返回汇总信息
 
 my_new_car = Car('audi','a4',2016) # 根据Car类创建了一个实例，将其存储到变量中
 print(my_new_car.get_descriptive_name()) # 实例.方法,调用方法
@@ -70,7 +70,7 @@ my_new_cars.read_odometer() # 实例.方法
 # 9.2.3 修改属性的值
 print("\n9.2.3")
 # 三种方法修改属性的值
-# 1.通过实例进行修改
+# 1.通过实例直接进行修改
 # 2.通过方法进行设置
 # 3.通过方法进行递增（增加特定的值）
 
@@ -92,14 +92,28 @@ my_car.odometer_readings = 2  # 直接修改属性的值，这里修改的是属
 my_car.read_odometers() # 这里调用的是方法，不是属性！！
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # 2.通过方法修改属性的值
 # 有一个更新属性的方法，就无需直接访问属性，将值传递给一个方法，由它在内部更新
 print("\n9.2.3.2")
 class   Carsa():
 
-    def __init__(self):
+    def __init__(self): # 属性已指定默认值
         """初始化描述汽车的属性"""
-        self.odometer_readings = 1
+        self.odometer_readings = 1 # 因为这里给属性指定了默认值，所以不需要有形参
 
     def read_odometers(self):
         """打印一条指出汽车里程的信息"""
@@ -124,4 +138,76 @@ my_car.update_odometer(10)     # 提示禁止修改
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # 3.通过方法对属性的值进行递增
+# 将属性值递增特定的量，而不是将其设置为全新的值
+# 一辆二手车，购买到登记期间增加了100英里
+print("\n9.2.3.3")
+
+class Car_b():
+    def __init__(self,make_b,model_b,year_b):
+        """汽车属性"""
+
+        #属性
+        self.make_b = make_b
+        self.model_b = model_b
+        self.year_b = year_b
+        self.odometer_reading_b = 0
+
+        #方法
+    def get_messages(self):   # 方法和方法缩进一致！！！！
+        zong = str(self.year_b) + ' ' + self.make_b + ' ' + self.model_b
+        return zong.title()
+
+    def read_odometer_b(self):
+        """打印一条汽车里程数"""
+        print("The car has " + str(self.odometer_reading_b) + ".")
+
+
+    def update_odometer_b(self,add):
+        """
+        将里程表读数设置为指定的值
+        禁止将里程表读数往回调
+        """
+
+        if add >= self.odometer_reading_b:
+                self.odometer_reading_b = add
+        else:
+                print("禁止修改")
+        
+    def increment_odometer(self,miles_b): # 新增一个方法，接受一个数字，将其加入到属性——self.odometer_b中 
+        """将里程表读数增加指定的量"""
+        self.odometer_reading_b += miles_b # 将获取到的实参的数字加入到属性self.odometer_reading_b中
+
+
+
+# 实例
+my_used_car = Car_b('audi','A1',2013) # 创建一个实例
+print(my_used_car.get_messages())
+
+my_used_car.update_odometer_b(23500) # 调用方法update，更新属性self.odometer_reading_b
+my_used_car.read_odometer_b()
+
+my_used_car.increment_odometer(100) # 调用方法increment，并加入到属性self.odometer_reading_b中
+my_used_car.read_odometer_b()
+
+
+    
+
