6ddca5d0fbef4e28027de32cfdd2e86c953b32aa
ydPro-G <46178109+ydPro-G@users.noreply.github.com>
2020-07-06 17:10:23 +0800

    使用json存储数据

--- python/10_file/10.1/read_file.py
+++ python/10_file/10.1/read_file.py
@@ -21,16 +21,14 @@ os.chdir('D:\\python_workbin')   # 修改文件的工作目录
 
 # 10.1.1  读取整个文件
 
-with open('pi_digits.txt') as file_object: # |函数open()接受一个参数：要打开的文件的名称
-                                           # |Python在当前工作路径查找该文件，也可以用绝对路径来指定
-                                           # |调用open()后，将一个表示文件及其内容的对象存储到变量file_object中
-                                           # |Python将这个对象存储在我们将在后面使用的变量中 
-                                            
-                                           # |关键字with在不再需要访问文件后将其关闭
+with open('pi_digits.txt') as file_object: # | with所求值的对象必须有一个enter()方法，一个exit()方法。
+                                           # | 紧跟with后面的语句被求值后，返回对象的enter()方法被调用，这个方法的返回值赋值到as后面的变量
+                                           # | 当with后面的代码块全部被执行完，将调用前面返回对象的exit()方法
 
 
-    contents = file_object.read() # 有了file.txt的文件对象后，使用方法read()读取这个文件全部内容，将其作为一个字符串存储到变量contents中
-    print(contents.rstrip()) # 打印变量的值可将这个文本文件全部内容显示出来
+
+    contents = file_object.read() # 使用方法read()读取这个文件全部内容，将其作为一个字符串存储到变量contents中
+    print(contents.rstrip()) 
   
   # 输出的文件末尾多了一个空行，因为read()到达文件末尾时返回一个空字符串，这个空字符串显示出来时一个空行
   # 空行可在print()语句中使用rstrip（）
---
+++ python/10_file/10.4/store.py
@@ -0,0 +1,130 @@
+# 10.4 存储数据
+
+ # 存储用户输入的信息
+ # 使用模块json来存储数据
+ # 模块json将python数据结构转储到文件中
+ # 还可以使用json在程序之间分享数据
+
+
+
+
+
+
+
+print('10.4.1')
+ # 10.4.1 使用json.dump()和json.load()
+
+  # 使用json.dump()来存储这组数字  （转储）
+  # 使用json.load()来读取这组数字  （负载）
+
+   # json.dump()接受两个实参，要存储的数据以及可用于存储数据的文件对象
+
+ # 导入模块json
+import json
+
+ # 创建一个数字列表
+numbers = [2,3,5,7,11,13]
+
+ # 指定数字列表要存储的文件名
+filename = 'numbers.json'
+
+ # 以写入模式 打开 文件
+with open(filename,'w') as f_obj:
+    # 使用json.dump()将数字列表转储到json中
+    json.dump(numbers,f_obj)
+
+
+
+ # 读取的时前面写入的文件 
+filename_load = 'numbers.json'
+
+ # 打开这个文件
+with open(filename) as f_load:
+    # 使用函数json.load()加载存储在文件中的信息，并存储到变量中
+    numbers_load = json.load(f_load)
+
+print(numbers_load)
+
+
+
+
+
+
+
+
+
+
+print('\n10.4.2')
+# 10.4.2 保存和读取用户生成的数据
+ #  保存用户生成的数据
+
+
+import json
+ # 将用户输入存储到变量中
+username = input('What is your name? ')
+ # 指定文件
+filenames = 'username.json'
+ # 打开文件，写入文件
+with open(filenames,'w') as file_object:
+
+    # 将用户输入的数据存储到username.json，dump(转储)
+    json.dump(username,file_object) 
+    print('We will remember you when you come back, ' + username + '!')
+
+
+
+# 使用load读取前面写入的文件
+
+import json
+
+filename_useload = 'username.json'
+
+with open(filename_useload) as file_uname:
+    # 使用load指定读取的文件，存储到变量中
+    userone = json.load(file_uname) 
+    print('Welcome back, ' + userone + "!")
+
+
+
+
+print('\n10.4.2.1')
+ # 使用try合并dump和load
+filename_try = 'username.json'  # 指定文件
+
+ # 异常
+try:
+
+    # 打开文件，将一系列操作存储到
+    with open(filename_try) as file_obj: # with后面的语句被求值后调用enter()方法，将enter方法返回值赋值到as后面的变量中
+        username_try = json.load(file_obj) # 文件存在就将其中数据读取到内存中
+
+ # 如果文件不存在，执行以下代码
+except FileNotFoundError:
+ # 获取用户输入，存储到变量中
+    username_try = input('What is your name?')
+
+    with open(filename_try,'w') as file_obj:
+        # 使用json.dump()将用户输入转储到文件和变量中
+        json.dump(username_try,filename_try)
+        print('我认为你会回来的，' + username_try + '!')
+
+else:
+    print('欢迎回来，' + username_try + '!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print('\n10.4.3')
+
+# 10.4.3  重构 
