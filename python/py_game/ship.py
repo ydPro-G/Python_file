6fb0f2a0201a98fc48427d5e12c05a892809cdfd
ydPro-G <46178109+ydPro-G@users.noreply.github.com>
2020-07-13 19:55:01 +0800

    update

---
+++ python/py_game/ship.py
@@ -0,0 +1,12 @@
+import pygame
+
+class Ship():
+    
+    def __init__(self,screen):
+        """初始化飞船并设置其初始位置"""
+        self.screen = screen
+
+        # 加载飞船图像并获取其外接矩形
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
