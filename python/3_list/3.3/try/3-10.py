92d26b548a0fd72b9c55644e5541c16003b833e2
ydPro-G <46178109+ydPro-G@users.noreply.github.com>
2020-07-31 18:40:56 +0800

    Update 3-10.py

--- python/3_list/3.3/try/3-10.py
+++ python/3_list/3.3/try/3-10.py
@@ -135,5 +135,17 @@ while True:
 
 
 # PS2： startswith()和endswith() 开始关联，结束关联
+
 'Hello world!'.startswith('Hello') # 开始关联
 'Hello world!'.startswith('world') # 结束关联
+
+
+
+
+
+
+
+# PS3: join()和split() 
+ #join 在一个字符串上调用，参数是一个字符串列表，返回一个字符串，返回字符串由传入的列表每个字符串连接而成
+ 'ABC'.join(['my','name','is','simple'])
+#  'MyABCnameABCisABCSimon'
