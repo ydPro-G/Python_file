f8ceae7eaf6068270cc612aa31f1f7b57865642a
ydPro-G <46178109+ydPro-G@users.noreply.github.com>
2020-09-19 16:57:01 +0800

    Update 4_9.py

--- python/python_100/1-10/4_9.py
+++ python/python_100/1-10/4_9.py
@@ -7,15 +7,26 @@ print(lst)
 
 # 5 定义有两个方法的类，从控制台输入获取字符串，打印字符串，打印大写字符串，包含简单的测试函数来测试类方法
 
-calss Iostring():
+class IOstring():
     def get_string(self):
-        self = input()
+        self.s = input()
+
     def print_string(self):
-        print(self.upper())
+        print(self.s.upper())
 
 xx = IOstring()
 xx.get_string()
 xx.print_string()
 
+
+# 6 编写一个程序，根据给定的公式计算并打印这个值
+# Q = Square root of [(2 _ C _ D)/H]
+# C is 50 H is 30 , D是一个变量，其值以,分割的序列输入到程序中
+from math import sqrt
+c = 50
+d = 30
+
+ def calc(D):
+     return sqrt((2*c*D)/H)
 
 
